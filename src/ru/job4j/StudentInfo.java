75e754840a76b5f7907daba30e35e8d31570197c
kolumb1 <tsvbip@yandex.ru>
2021-09-02 13:24:41 +0300

    3.3. Разбор первой программы. [#159033]

--- src/ru/job4j/StudentInfo.java
+++ src/ru/job4j/StudentInfo.java
@@ -3,6 +3,6 @@ package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Artem");
-        System.out.println("01.01.1900");
+        System.out.println("01.01.1901");
     }
 }
